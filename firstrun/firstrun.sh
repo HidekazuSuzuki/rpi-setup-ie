06f9ca4caf5bdf1c8ce1e15fcb03eeae741b3b8b
Hidekazu Suzuki <hsuzuki@ucl.meijo-u.ac.jp>
2024-06-12 19:46:22 +0900

    Create firstrun.sh
    Raspberry Pi Imagerで作成したmicroSD/USBフラッシュメモリのbootfsにあるfirstrun.shの初期バージョン（Raspberry Pi OS 64-bit Bookworm）
    
    【設定内容】
    - ホスト名：rpi4-xxx
    - ユーザ名：ie-user
    - パスワード：ie-user2024
    - Wi-Fi：
    * SSID：UCLab
    * Passphrase：研究室のもの（セキュリティ上の理由により{PSK_VALUE}に置き換え）
    - SSH：有効
    - タイムゾーン：Asia/Tokyo

---
+++ firstrun/firstrun.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set +e
+
+CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_hostname rpi4-xxx
+else
+   echo rpi4-xxx >/etc/hostname
+   sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\trpi4-xxx/g" /etc/hosts
+fi
+FIRSTUSER=`getent passwd 1000 | cut -d: -f1`
+FIRSTUSERHOME=`getent passwd 1000 | cut -d: -f6`
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom enable_ssh
+else
+   systemctl enable ssh
+fi
+if [ -f /usr/lib/userconf-pi/userconf ]; then
+   /usr/lib/userconf-pi/userconf 'ie-user' '$5$6QPxmcGbbd$1/CtRMdtM2sKhLmk4BqZySAEAOB4wFeM3S96rSBkS27'
+else
+   echo "$FIRSTUSER:"'$5$6QPxmcGbbd$1/CtRMdtM2sKhLmk4BqZySAEAOB4wFeM3S96rSBkS27' | chpasswd -e
+   if [ "$FIRSTUSER" != "ie-user" ]; then
+      usermod -l "ie-user" "$FIRSTUSER"
+      usermod -m -d "/home/ie-user" "ie-user"
+      groupmod -n "ie-user" "$FIRSTUSER"
+      if grep -q "^autologin-user=" /etc/lightdm/lightdm.conf ; then
+         sed /etc/lightdm/lightdm.conf -i -e "s/^autologin-user=.*/autologin-user=ie-user/"
+      fi
+      if [ -f /etc/systemd/system/getty@tty1.service.d/autologin.conf ]; then
+         sed /etc/systemd/system/getty@tty1.service.d/autologin.conf -i -e "s/$FIRSTUSER/ie-user/"
+      fi
+      if [ -f /etc/sudoers.d/010_pi-nopasswd ]; then
+         sed -i "s/^$FIRSTUSER /ie-user /" /etc/sudoers.d/010_pi-nopasswd
+      fi
+   fi
+fi
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_wlan 'UCLab' '{PSK_VALUE}' 'JP'
+else
+cat >/etc/wpa_supplicant/wpa_supplicant.conf <<'WPAEOF'
+country=JP
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+ap_scan=1
+
+update_config=1
+network={
+	ssid="UCLab"
+	psk={PSK_VALUE}
+}
+
+WPAEOF
+   chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
+   rfkill unblock wifi
+   for filename in /var/lib/systemd/rfkill/*:wlan ; do
+       echo 0 > $filename
+   done
+fi
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_keymap 'jp'
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_timezone 'Asia/Tokyo'
+else
+   rm -f /etc/localtime
+   echo "Asia/Tokyo" >/etc/timezone
+   dpkg-reconfigure -f noninteractive tzdata
+cat >/etc/default/keyboard <<'KBEOF'
+XKBMODEL="pc105"
+XKBLAYOUT="jp"
+XKBVARIANT=""
+XKBOPTIONS=""
+
+KBEOF
+   dpkg-reconfigure -f noninteractive keyboard-configuration
+fi
+rm -f /boot/firstrun.sh
+sed -i 's| systemd.run.*||g' /boot/cmdline.txt
+exit 0
